63773ba835b8d6cc671f49ab5a1e1dd5886fd122
Cyunrei <cyunrei@gmail.com>
2020-10-25 12:28:16 +0800

    Add shell css backup file recovery script

---
+++ recovery.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Restore from backup file"
+sudo cp -f /usr/share/gnome-shell/theme/Flat-Remix-Blue/gnome-shell.css.bak /usr/share/gnome-shell/theme/Flat-Remix-Blue/gnome-shell.css
+echo "Recovery finish!"
